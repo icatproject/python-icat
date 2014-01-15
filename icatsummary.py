178b04d9358dae5d3440f622879c9adcf34173e9
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2014-01-15 12:37:23 +0000

    Add example icatsummary.py
    git-svn-id: http://manti.exp.bessy.de/svn/icat/python-client/trunk@218 8efdbd46-c5fb-49ab-9956-99f62928ec21

---
+++ icatsummary.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+#
+# Search the ICAT for all entity types and report the number of
+# entrities found for each type.
+#
+
+import icat
+import icat.config
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('suds.client').setLevel(logging.CRITICAL)
+
+icat.config.defaultsection = "hzb"
+conf = icat.config.Config().getconfig()
+
+client = icat.Client(conf.url, **conf.client_kwargs)
+client.login(conf.auth, conf.credentials)
+
+
+print "Connect to %s" % conf.url
+print "User: %s" % client.getUserName()
+print
+
+entitycolwidth = 24
+print "%-*s   %s" % (entitycolwidth, "Entity", "count")
+print "-" * (entitycolwidth + 3 + 5)
+for entityname in client.getEntityNames():
+    try:
+        res = client.search(entityname)
+    except icat.exception.ICATPrivilegesError:
+        # ICAT 4.2.* raises a PrivilegesError if there are entities
+        # matching the search but the user has no read permission to
+        # any of them.  ICAT 4.3.* returns an empty list.  See ICAT
+        # Issue 120.
+        res = []
+    print "%-*s : %d" % (entitycolwidth, entityname, len(res))
+print
