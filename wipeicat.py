22e09812c0d97157908b43a93e36fbd1e9aadbda
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2014-11-25 16:47:50 +0000

    Must restore datafiles before they can be deleted from IDS.
    git-svn-id: http://manti.exp.bessy.de/svn/icat/python-client/trunk@549 8efdbd46-c5fb-49ab-9956-99f62928ec21

--- wipeicat.py
+++ wipeicat.py
@@ -3,9 +3,11 @@
 # Delete all content from an ICAT.
 #
 
-import icat
-import icat.config
+import time
 import logging
+import icat
+from icat.ids import DataSelection
+import icat.config
 
 logging.basicConfig(level=logging.INFO)
 #logging.getLogger('suds.client').setLevel(logging.DEBUG)
@@ -33,6 +35,16 @@ def deletetype(t):
                 break
             client.deleteMany(objs)
 
+def waitOpsQueue():
+    """Wait for the opsQueue in the service status to drain.
+    """
+    while True:
+        status = client.ids.getServiceStatus()
+        if len(status['opsQueue']) == 0:
+            break
+        time.sleep(30)
+
+
 # Entity types needed for access permissions.  Must be deleted last,
 # otherwise we would take delete permission from ourselves.
 if client.apiversion < '4.3':
@@ -43,21 +55,49 @@ else:
 
 # First step, delete all Datafiles from IDS.
 # 
-# There is a problem: if the Datafile has been created with IDS by a
-# file upload then we MUST delete it with IDS, otherwise it would
+# This is somewhat tricky: if the Datafile has been created with IDS
+# by a file upload then we MUST delete it with IDS, otherwise it would
 # leave an orphan file in the IDS.  If the Datafile has been created
-# in the ICAT client without IDS, we MUST NOT delete it with IDS,
-# because IDS will not find the actual file and will throw an internal
-# server error.  But there is no reliable way to tell the one from the
-# other.  As a rule, we will assume that the file has been created
-# with IDS if the location attribute is set.  But of course, there is
-# no way to prevent you from creating the file in the ICAT client and
-# set a location ...
+# in the ICAT client without IDS, we cannot delete it with IDS,
+# because IDS will not find the actual file and will throw a server
+# error.  But there is no reliable way to tell the one from the other.
+# As a rule, we will assume that the file has been created with IDS if
+# the location attribute is set.  Furthermore, we must restore the
+# datasets first, because IDS can only delete datafiles that are
+# online.  But restoring one dataset may cause another one to get
+# archived if free main storage is low.  So we might need several
+# sweeps to get everything deleted.  In each sweep, we delete
+# everything that is currently online in a first step and file a
+# restore request for all the rest in a second step.
 if client.ids:
-    datafileIds = client.search("SELECT d.id from Datafile d "
-                                "WHERE d.location IS NOT NULL")
-    if datafileIds:
-        client.deleteData(dict(datafileIds=datafileIds))
+    if client.apiversion < '4.3':
+        searchexp = "Dataset INCLUDE Datafile"
+    else:
+        searchexp = "SELECT o FROM Dataset o INCLUDE o.datafiles LIMIT 0, 100"
+    while True:
+        restsel = DataSelection()
+        deleted = False
+        # Wait for the server to process all pending requests.  This
+        # may be needed to avoid race conditions, see
+        # https://code.google.com/p/icat-data-service/issues/detail?id=14
+        waitOpsQueue()
+        for ds in client.search(searchexp):
+            selection = DataSelection([df for df in ds.datafiles 
+                                       if df.location is not None])
+            if selection:
+                if client.ids.getStatus(selection) == "ONLINE":
+                    client.deleteData(selection)
+                    deleted = True
+                else:
+                    restsel.extend(selection)
+            else:
+                client.delete(ds)
+                deleted = True
+        if not deleted and not restsel:
+            break
+        if restsel:
+            client.ids.restore(restsel)
+
 
 # Second step, delete the Facility.  By cascading, this already wipes
 # almost everything.
