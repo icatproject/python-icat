5b68aec4e30ec3169617cb23b35ad2ac654883ee
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2014-12-01 10:46:15 +0000

    Report also the Python version.
    git-svn-id: http://manti.exp.bessy.de/svn/icat/python-client/trunk@554 8efdbd46-c5fb-49ab-9956-99f62928ec21

--- getversion.py
+++ getversion.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python
 
 from __future__ import print_function
+import sys
+import logging
 import icat
 import icat.config
-import logging
 
 logging.basicConfig(level=logging.INFO)
 #logging.getLogger('suds.client').setLevel(logging.DEBUG)
@@ -11,5 +12,6 @@ logging.basicConfig(level=logging.INFO)
 conf = icat.config.Config(needlogin=False).getconfig()
 
 client = icat.Client(conf.url, **conf.client_kwargs)
+print("Python %s\n" % (sys.version))
 print("python-icat version %s (%s)\n" % (icat.__version__, icat.__revision__))
 print("Connect to %s\nICAT version %s\n" % (conf.url, client.apiversion))
