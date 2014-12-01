b3b0b50b43a7c85f0d61b71a48c641e259a4eed1
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2014-12-01 09:41:27 +0000

    Report also the python-icat version.
    git-svn-id: http://manti.exp.bessy.de/svn/icat/python-client/trunk@551 8efdbd46-c5fb-49ab-9956-99f62928ec21

--- getversion.py
+++ getversion.py
@@ -11,4 +11,5 @@ logging.basicConfig(level=logging.INFO)
 conf = icat.config.Config(needlogin=False).getconfig()
 
 client = icat.Client(conf.url, **conf.client_kwargs)
+print("python-icat version %s (%s)\n" % (icat.__version__, icat.__revision__))
 print("Connect to %s\nICAT version %s\n" % (conf.url, client.apiversion))
