9486a6a3dddece5d7b636e54d3cbc59436206a65
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2015-05-07 11:23:31 +0000

    Connect also to the IDS (if idsurl is set) and report its version.
    git-svn-id: http://manti.exp.bessy.de/svn/icat/python-client/trunk@844 8efdbd46-c5fb-49ab-9956-99f62928ec21

--- getversion.py
+++ getversion.py
@@ -9,9 +9,12 @@ import icat.config
 logging.basicConfig(level=logging.INFO)
 #logging.getLogger('suds.client').setLevel(logging.DEBUG)
 
-conf = icat.config.Config(needlogin=False).getconfig()
+conf = icat.config.Config(needlogin=False, ids="optional").getconfig()
 
 client = icat.Client(conf.url, **conf.client_kwargs)
 print("Python %s\n" % (sys.version))
 print("python-icat version %s (%s)\n" % (icat.__version__, icat.__revision__))
 print("Connect to %s\nICAT version %s\n" % (conf.url, client.apiversion))
+if client.ids:
+    print("Connect to %s\nIDS version %s\n" 
+          % (conf.idsurl, client.ids.apiversion))
