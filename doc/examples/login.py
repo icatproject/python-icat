ac35bac136224026426f77dd9344f1d481acb5bd
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2017-06-07 15:53:55 +0200

    Split the calls to the Config constructor and getconfig().

--- doc/examples/login.py
+++ doc/examples/login.py
@@ -8,7 +8,8 @@ import logging
 logging.basicConfig(level=logging.INFO)
 #logging.getLogger('suds.client').setLevel(logging.DEBUG)
 
-client, conf = icat.config.Config(ids="optional").getconfig()
+config = icat.config.Config(ids="optional")
+client, conf = config.getconfig()
 sessionId = client.login(conf.auth, conf.credentials)
 
 print("Login to", conf.url, "was successful.")
