aa9271c4869cc395bccb8baad731497e5cc85f99
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2020-05-07 17:38:00 +0200

    Include the related Grouping and the PublicStep in the output of dumprules.py example script.

--- doc/examples/dumprules.py
+++ doc/examples/dumprules.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 #
-# Dump the rules from the ICAT to a file or to stdout.
+# Dump the rules including the related groups and the public steps
+# from the ICAT to a file or to stdout.
 #
 
 import logging
@@ -33,12 +34,22 @@ config.add_variable('format', ("-f", "--format"),
 client, conf = config.getconfig()
 client.login(conf.auth, conf.credentials)
 
+groups = set()
+query = Query(client, "Rule",
+              conditions={"grouping": "IS NOT NULL"},
+              includes={"grouping.userGroups.user"})
+for r in client.search(query):
+    groups.add(r.grouping)
 
-rules = [Query(client, "Rule", order=["what", "id"], 
-               conditions={"grouping":"IS NULL"}), 
-         Query(client, "Rule", order=["grouping.name", "what", "id"], 
-               conditions={"grouping":"IS NOT NULL"}, 
-               includes={"grouping"}) ]
+items = [
+    sorted(groups, key=icat.entity.Entity.__sortkey__),
+    Query(client, "PublicStep"),
+    Query(client, "Rule", order=["what", "id"],
+          conditions={"grouping": "IS NULL"}),
+    Query(client, "Rule", order=["grouping.name", "what", "id"],
+          conditions={"grouping": "IS NOT NULL"},
+          includes={"grouping"}),
+]
 
 with open_dumpfile(client, conf.file, conf.format, 'w') as dumpfile:
-    dumpfile.writedata(rules)
+    dumpfile.writedata(items)
