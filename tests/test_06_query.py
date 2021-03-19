8e1b40baac101317c1804828f18247deb39b98ef
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2021-03-19 19:41:18 +0100

    Add tests for deprecated features of class Query

--- tests/test_06_query.py
+++ tests/test_06_query.py
@@ -480,6 +480,27 @@ def test_query_mulitple_attributes_related_obj(client):
     res = client.search(query)
     assert res == results
 
+def test_query_deprecated_kwarg_attribute(client):
+    """the keyword argument `attribute` to :class:`icat.query.Query`
+    is deprecated since 0.18.0.
+    """
+    # create a reference using the new keyword argument
+    ref_query = Query(client, "Datafile", attributes="name")
+    with pytest.deprecated_call():
+        query = Query(client, "Datafile", attribute="name")
+    assert str(query) == str(ref_query)
+
+def test_query_deprecated_method_setAttribute(client):
+    """:meth:`icat.query.Query.setAttribute` is deprecated since 0.18.0.
+    """
+    # create a reference using the new method
+    ref_query = Query(client, "Datafile")
+    ref_query.setAttributes("name")
+    with pytest.deprecated_call():
+        query = Query(client, "Datafile")
+        query.setAttribute("name")
+    assert str(query) == str(ref_query)
+
 @pytest.mark.dependency(depends=['get_investigation'])
 def test_query_aggregate_distinct_attribute(client):
     """Test DISTINCT on an attribute in the search result.
