485a0b26ad28d91a91c9887dda33488ff559666c
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2022-08-13 17:17:34 +0200

    Adapt to distutils-pytest 0.2

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ import string
 import sys
 try:
     import distutils_pytest
-except ImportError:
-    pass
+    cmdclass = distutils_pytest.cmdclass
+except (ImportError, AttributeError):
+    cmdclass = dict()
 try:
     import setuptools_scm
     version = setuptools_scm.get_version()
@@ -174,10 +175,9 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    cmdclass = {
-        'build_py': build_py,
-        'build_test': build_test,
-        'init_py': init_py,
-        'sdist': sdist,
-    },
+    cmdclass = dict(cmdclass,
+                    build_py=build_py,
+                    build_test=build_test,
+                    init_py=init_py,
+                    sdist=sdist),
 )
