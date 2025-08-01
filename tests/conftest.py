f6a193d31181f1dc73ddcdf6004d05706ef5d431
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2025-08-01 14:16:58 +0200

    Formally add example and test reference data for ICAT 6.2, not yet adapting the content, though

--- tests/conftest.py
+++ tests/conftest.py
@@ -177,6 +177,8 @@ def require_dumpfile_backend(backend):
     if backend not in icat.dumpfile.Backends.keys():
         _skip("need %s backend for icat.dumpfile" % (backend))
 
+# FIXME: replace all references to icat_version "6.2a0" below by "6.2"
+# as soon as icat.server 6.2 has been released.
 
 def get_icatdata_schema():
     if icat_version < "4.4":
@@ -187,8 +189,10 @@ def get_icatdata_schema():
         fname = "icatdata-4.7.xsd"
     elif icat_version < "5.0":
         fname = "icatdata-4.10.xsd"
-    else:
+    elif icat_version < "6.2a0":
         fname = "icatdata-5.0.xsd"
+    else:
+        fname = "icatdata-6.2.xsd"
     return gettestdata(fname)
 
 
@@ -200,16 +204,20 @@ def get_reference_dumpfile(ext = "yaml"):
         fname = "icatdump-4.7.%s" % ext
     elif icat_version < "5.0":
         fname = "icatdump-4.10.%s" % ext
-    else:
+    elif icat_version < "6.2a0":
         fname = "icatdump-5.0.%s" % ext
+    else:
+        fname = "icatdump-6.2.%s" % ext
     return gettestdata(fname)
 
 
 def get_reference_summary():
     if icat_version < "5.0":
         version_suffix = "4"
-    else:
+    elif icat_version < "6.2a0":
         version_suffix = "5"
+    else:
+        version_suffix = "6.2"
     users = [ "acord", "ahau", "jbotu", "jdoe", "nbour", "rbeck" ]
     refsummary = { "root": gettestdata("summary-%s" % version_suffix) }
     for u in users:
